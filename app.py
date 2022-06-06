b87857cf3171d222b51ba7d91ec863e5cf3300b5
Nick Jackson <njnickjackson@yahoo.com>
2022-06-06 15:20:47 -0400

    First Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for, redirect, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("home_page.html")
+
+
+@app.route('/HelpMeChoose')
+def choose():
+    return render_template("help_choose.html")
+
+
+if __name__ == '__main__':
+    app.run()
